236ba90128e309d8b7a68af52f56030028323676
Katerina Boufea <s1756111@igmm-mac-s1756111.med.ed.ac.uk>
2019-01-25 13:34:51 +0000

    Remove initialization of GMM parameters

--- R/fit_GMM.R
+++ R/fit_GMM.R
@@ -4,11 +4,7 @@ final_populations <- function(score, likelihood_threshold) {
   if (is.null(results)) {
     return(c())
   } else {
-    if (is.null(lambda_est) && is.null(mean_est) && is.null(sd_est)) {
-      mixmdl <- mixtools::normalmixEM(score, k=2, maxit = 10000, fast = TRUE)
-    } else {
-      mixmdl <- mixtools::normalmixEM(score, k=2, maxit = 10000, fast = TRUE, lambda = lambda_est, mu = mean_est, sigma = sd_est)
-    }
+    mixmdl <- mixtools::normalmixEM(score, k=2, maxit = 10000, fast = TRUE)
     # Ask user to choose allowed contamination percentage
     matches <- intersect(names(score)[which(mixmdl$posterior[, which(mixmdl$mu == max(mixmdl$mu))] >= likelihood_threshold)], 
                     names(score)[which(score >= min(mixmdl$mu))])
