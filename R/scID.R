691386bcfd25d94257a9649156472a809c90aab7
Katerina Boufea <s1756111@igmm-mac-s1756111.med.ed.ac.uk>
2019-01-25 16:54:47 +0000

    Added progress bars

--- R/scID.R
+++ R/scID.R
@@ -11,29 +11,17 @@ scid_match_cells <- function(target_gem = NULL, reference_gem = NULL, reference_
                              logFC = 0.5, use_reference_for_weights = FALSE, likelihood_threshold = 0.99){
   
   #----------------------------------------------------------------------------------------------------
+  # Data pre-processing
   # Check all reference cells have a cluster ID
   common_cells <- intersect(names(reference_clusters), colnames(reference_gem))
   if (length(common_cells) == 0) {
-    print("None  of the reference cell has a cluster ID. Please check the reference_clusters list provided.")
+    print("None  of the reference cells has a cluster ID. Please check the reference_clusters list provided.")
     return()
   } else {
     reference_gem <- reference_gem[, common_cells]
     reference_clusters <- reference_clusters[common_cells]
   }
-  # Find signature genes from reference data
-  so_ref <- Seurat::CreateSeuratObject(raw.data = reference_gem)
-  so_ref <- Seurat::NormalizeData(so_ref)
-  so_ref <- Seurat::ScaleData(so_ref)
-  so_ref@ident <- as.factor(reference_clusters)
   
-  markers <- Seurat::FindAllMarkers(so_ref, test.use = "MAST", only.pos = TRUE, logfc.threshold = logFC)
-  
-  # Filter out signature genes that are not present in the target data
-  markers <- markers[which(markers$gene %in% rownames(target_gem)), ]
-  
-  celltypes <- unique(markers$cluster)
-  
-  #----------------------------------------------------------------------------------------------------
   # Min-max normalization of target gem
   target_gem_norm <- t(apply(target_gem[markers$gene, ], 1, function(x) normalize_gem(x)))
   na.values <- which(apply(target_gem_norm, 1, function(x){any(is.na(x))}))
@@ -42,8 +30,27 @@ scid_match_cells <- function(target_gem = NULL, reference_gem = NULL, reference_
   }
   
   #----------------------------------------------------------------------------------------------------
-  # Weight signature genes
+  print("Stage 1: extract signatures genes from reference clusters")
+  
+  # Stage 1: Find signature genes from reference data
+  so_ref <- Seurat::CreateSeuratObject(raw.data = reference_gem)
+  so_ref <- Seurat::NormalizeData(so_ref)
+  so_ref <- Seurat::ScaleData(so_ref)
+  so_ref@ident <- as.factor(reference_clusters)
+  
+  markers <- suppressMessages(Seurat::FindAllMarkers(so_ref, test.use = "MAST", only.pos = TRUE, logfc.threshold = logFC))
+  
+  # Filter out signature genes that are not present in the target data
+  markers <- markers[which(markers$gene %in% rownames(target_gem)), ]
+  
+  celltypes <- unique(markers$cluster)
+  
+  #----------------------------------------------------------------------------------------------------
+  # Stage 2: Weight signature genes
+  print("Stage 2: Estimate weights of signature genes")
+  
   weights <- list()
+  
   if (use_reference_for_weights) {
     #Min-max normalization of reference gem
     ref_gem_norm <-  t(apply(reference_gem[markers$gene, ], 1, function(x) normalize_gem(x)))
@@ -51,48 +58,69 @@ scid_match_cells <- function(target_gem = NULL, reference_gem = NULL, reference_
     if (length(na.values > 0)) {
       ref_gem_norm <- ref_gem_norm[-na.values, ]
     }
-    for (celltype in celltypes) {
-      signature_genes <- markers$gene[which(markers$cluster == celltype)]
-      IN <- names(which(reference_clusters == celltype))
+    
+    for (i in 1:length(celltypes)) {
+      #svMisc::progress(i, max.value = length(celltypes), progress.bar = T)
+      svMisc::progress(i*100/length(celltypes), max.value = 100, char = "%")
+      Sys.sleep(0.01)
+      signature_genes <- markers$gene[which(markers$cluster == celltypes[i])]
+      IN <- names(which(reference_clusters == celltypes[i]))
       OUT <- setdiff(colnames(reference_gem), IN)
       gene.weights <- scID_weight(ref_gem_norm, IN, OUT)
-      weights[[celltype]] <- gene.weights
+      weights[[celltypes[i]]] <- gene.weights
+      if (i==length(celltypes)) cat("Done!")
     }
+    
   } else {
-    for (celltype in celltypes) {
-      signature_genes <- markers$gene[which(markers$cluster == celltype)]
+    for (i in 1:length(celltypes)) {
+      svMisc::progress(i*100/length(celltypes), max.value = 100, char = "%")
+      Sys.sleep(0.01)
+      signature_genes <- markers$gene[which(markers$cluster == celltypes[i])]
       putative_groups <- choose_unsupervised(target_gem, signature_genes)
       gene.weights <- scID_weight(target_gem_norm[signature_genes, ], putative_groups$in_pop, putative_groups$out_pop)
-      weights[[celltype]] <- gene.weights
+      weights[[celltypes[i]]] <- gene.weights
+      if (i==length(celltypes)) cat("Done!")
     }
   }
   
   #----------------------------------------------------------------------------------------------------
+  # Stage 3
   # Find scores and putative matches
+  print("Stage 3.1-2: Calculate scores and find matching cells")
+  
   scores <- data.frame(matrix(NA, length(celltypes), ncol(target_gem)), row.names = celltypes)
   colnames(scores) <- colnames(target_gem)
-  for (celltype in celltypes) {
+  
+  for (i in 1:length(celltypes)) {
+    celltype <- as.integer(celltypes[i])
+    svMisc::progress(i*100/length(celltypes), max.value = 100)
+    Sys.sleep(0.01)
     signature <- names(weights[[celltype]])
     weighted_gem <- weights[[celltype]] * target_gem_norm[signature, ]
     score <- colSums(weighted_gem)/sum(weights[[celltype]])
-  matches <- final_populations(score, likelihood_threshold)
+    matches <- final_populations(score, likelihood_threshold) # NEED TO SUPPRESS THE MESSAGE
     scores[celltype, matches] <- scale(score[matches])
+    if (i==length(celltypes)) cat("Done!")
   }
   
   # Resolve multiclass assignments
+  print ("Stage 3.3: Resolve multiclass assignments")
   labels <- c()
-  for (cell in colnames(scores)) {
+  for (i in 1:ncol(scores)) {
+    svMisc::progress(i*100/ncol(scores), max.value = 100)
+    Sys.sleep(0.01)
+    cell <- colnames(scores)[i]
     if (all(is.na(scores[, cell]))) {
       matching_type <- "unassigned"
     } else {
       matching_type <- rownames(scores)[which(scores[, cell] == max(scores[, cell], na.rm = T))]
     }
     labels <- c(labels, matching_type)
+    if (i==ncol(scores)) cat("Done!")
   }
   names(labels) <- colnames(scores)
   table(labels)
 
-  #----------------------------------------------------------------------------------------------------
   # return result
   return(labels)
     
