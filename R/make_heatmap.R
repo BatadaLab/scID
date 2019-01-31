69e4ceff55e3db82b54032ed17b9fe2b600e3869
Katerina Boufea <s1756111@igmm-mac-s1756111.med.ed.ac.uk>
2019-01-31 15:52:44 +0000

    Added function to plot heatmap of clustered data

---
+++ R/make_heatmap.R
@@ -0,0 +1,27 @@
+#' @export
+make_heatmap <- function(gem, labels, markers) {
+  
+  celltypes <- unique(markers$cluster)
+  
+  gem_avg <- matrix(NA, length(celltypes), length(celltypes))
+  for (i in 1:length(celltypes)) {
+    cells <- na.omit(names(labels)[which(labels == celltypes[i])])
+    if (length(cells) > 1) {
+      avg_exp <- rowMeans(gem[toupper(markers$gene), cells])
+    } else if (length(cells) == 1) {
+      avg_exp <- gem[toupper(markers$gene), cells]
+      names(avg_exp) <- toupper(markers$gene)
+    } else {
+      next
+    }
+    for (j in 1:length(celltypes)) {
+      gem_avg[j,i] <- mean(na.omit(avg_exp[toupper(markers$gene[which(markers$cluster == celltypes[j])])]))
+    }
+  }
+  
+  rownames(gem_avg) <- celltypes
+  colnames(gem_avg) <- celltypes
+  
+  pheatmap::pheatmap(gem_avg, border="white", #color = colorspace::diverge_hcl(50, h=c(180, 70), c=70, l=c(70, 90)), 
+                     cluster_rows = F, cluster_cols = F, border_color = F, scale = "row")
+}
