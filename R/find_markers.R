2b0d1c5e75449e60dee46692b2083bc29612b98d
Katerina Boufea <katerina.boufea@outlook.com>
2019-05-10 17:50:06 +0100

    Compatible with Seurat v3

--- R/find_markers.R
+++ R/find_markers.R
@@ -1,11 +1,12 @@
 #' @export
 find_markers <- function(reference_gem, reference_clusters, logFC) {
-  so_ref <- Seurat::CreateSeuratObject(raw.data = reference_gem)
-  so_ref <- suppressMessages(Seurat::NormalizeData(so_ref))
-  so_ref <- suppressMessages(Seurat::ScaleData(so_ref))
-  so_ref@ident <- as.factor(reference_clusters)
+  library(Seurat)
+  so_ref <- CreateSeuratObject(counts = reference_gem)
+  so_ref <- suppressMessages(NormalizeData(so_ref))
+  so_ref <- suppressMessages(ScaleData(so_ref))
+  Idents(so_ref) <- as.factor(reference_clusters)
   
-  markers <- suppressMessages(Seurat::FindAllMarkers(so_ref, test.use = "MAST", only.pos = TRUE, logfc.threshold = logFC))
+  markers <- suppressMessages(FindAllMarkers(so_ref, test.use = "MAST", only.pos = TRUE, logfc.threshold = logFC))
   
   return(markers)
 }
