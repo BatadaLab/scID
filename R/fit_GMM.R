63a210d4b4dcebe3044f16fbc9c77b45e0623983
Katerina Boufea <katerina.boufea@outlook.com>
2019-05-17 17:18:46 +0100

    Update fit_GMM.R

--- R/fit_GMM.R
+++ R/fit_GMM.R
@@ -2,20 +2,6 @@
 #' @param score List of scID scores for target cells
 #' @return List of matching cells
 #' @export
-<<<<<<< HEAD
-final_populations <- function(score, likelihood_threshold) {
-  results <- tryCatch(mixtools::normalmixEM(score, k=2, fast = TRUE, verb = FALSE),error = function(e) {return(NULL)})
-  if (is.null(results)) {
-    return(c())
-  } else {
-    mixmdl <- mixtools::normalmixEM(score, k=2, maxit = 10000, fast = TRUE, verb = F)
-    
-    # Ask user to choose allowed contamination percentage
-    matches <- intersect(names(score)[which(mixmdl$posterior[, which(mixmdl$mu == max(mixmdl$mu))] >= likelihood_threshold)], 
-                         names(score)[which(score >= min(mixmdl$mu))])
-    
-    return(matches)
-=======
 
 final_populations <- function(score) {
   
@@ -26,15 +12,8 @@ final_populations <- function(score) {
   names(avgScore) <- unique(fit$classification)
   for (ID in names(avgScore)) {
     avgScore[ID] <- mean(score[names(which(fit$classification == ID))])
->>>>>>> develop
   }
   
   matches <- names(fit$classification)[which(fit$classification == names(which(avgScore == max(avgScore))))]
   
 }
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> develop
