66f683d09e213496165436146ef13dc1300e6889
Katerina Boufea <s1756111@igmm-mac-s1756111.med.ed.ac.uk>
2019-05-29 15:31:12 +0100

    Updated make_heatmap function

--- R/plots_functions.R
+++ R/plots_functions.R
@@ -22,7 +22,7 @@ make_heatmap <- function(gem, labels, markers) {
   # Keep markers present in gem
   markers <- markers[which(markers$gene %in% rownames(gem)), ]
   
-  celltypes <- unique(markers$cluster)
+  celltypes <- c(unique(markers$cluster), unique(labels))
   
   gem_avg <- matrix(NA, length(celltypes), length(celltypes))
   for (i in 1:length(celltypes)) {
@@ -37,8 +37,12 @@ make_heatmap <- function(gem, labels, markers) {
     }
   }
   
-  rownames(gem_avg) <- paste("gs", unique(markers$cluster), sep = "_")
+  rownames(gem_avg) <- paste("gs", celltypes, sep = "_")
   colnames(gem_avg) <- paste("Cl", celltypes, sep = "_")
+  # remove columns that are all NA
+  gem_avg <- gem_avg[, -which(apply(gem_avg, 2, function(x) all(is.na(x))))]
+  # remove rows that are all NA
+  gem_avg <- gem_avg[-which(apply(gem_avg, 1, function(x) all(is.na(x)))), ]
   
   pheatmap::pheatmap(gem_avg, border="white", cluster_rows = F, cluster_cols = F, border_color = F, scale = "row")
 }
